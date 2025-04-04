760dda8fbd01c2f2af651f464bbe5704f8b56156
MDR57 <riomardiego@gmail.com>
2025-04-04 08:59:27 +0200

    1

--- src/Conexion.java
+++ src/Conexion.java
@@ -1,18 +1,41 @@
 import java.sql.*;
+import java.util.Scanner;
+import java.sql.Connection;
 
 public class Conexion{
     public static void main(String[] args) {
         String url = "jdbc:mysql://25.7.162.154:3306/bdbiblioteca";
         String user = "admin";
         String password = "1234";
+        Scanner teclado=new Scanner(System.in);
 
-        try {
-            // Conexión a la base de datos
-            Connection conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Conexión exitosa a la base de datos.");
-            conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+        System.out.println("*********************************************************************");
+        System.out.println("***/                    BIBLIOTECA MUSKIZ                        /***");
+        System.out.println("*********************************************************************\n");
+        System.out.println("       1-Obtener conexión                 4-Cerrar conexión");
+
+
+        int eleccion = teclado.nextInt();
+
+        switch (eleccion) {
+            case 1:
+                // Conexión a la base de datos
+                conn = BaseDeDatos.ConexionBaseDeDatos(url, user, password);
+                if (conn == null) {
+                    BaseDeDatos.mensa("No se pudo establecer la conexión");
+                    return;
+                }
+                System.out.println("Conexión exitosa a la base de datos.");
+                break;
+        
+
+            case 4:
+                BaseDeDatos.cerrarConexion(conn);
+                BaseDeDatos.mensa("Conexión cerrada exitosamente!");
+                break;
+            default:
+                BaseDeDatos.mensa("Elige una de las opciones que se dan.");
+                break;
         }
     }
 }
