1fe9251b2190c9f512bef913c64f0dc7b3260a2d
Asier <asier.uriarte06@somo.eus>
2025-04-04 13:36:08 +0200

    cambios hechos
    queda pendiente arreglarlo

--- src/Conexion.java
+++ src/Conexion.java
@@ -4,13 +4,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Conexion{
-        // Códigos ANSI para colores
-        public static final String RESET = "\u001B[0m";
-        public static final String ROJO = "\u001B[31m";
-        public static final String VERDE = "\u001B[32m";
-        public static final String AMARILLO = "\u001B[33m";
-        public static final String AZUL = "\u001B[34m";
-        public static final String FONDO_AZUL = "\u001B[44m";
     public static void main(String[] args) {
         String url = "jdbc:mysql://25.7.162.154:3306/bdbiblioteca";
         String user = "admin";
@@ -59,46 +52,9 @@ public class Conexion{
                     break;
     
                 case 10:
-                    Function.cls();
-                    System.out.println("***********************");
-                    System.out.println("**/     colores     /**");
-                    System.out.println("***********************");
-                    System.out.println("1-Verde");
-                    System.out.println("2-Azul");
-                    System.out.println("3-Rojo");
-                    System.out.println("4-Amarillo");
-                    System.out.println("5-Fondo azul");
-                    System.out.println("6-Resetear");
-                    eleccion = teclado.nextInt();
-                    switch (eleccion) {
-                        case 1:
-                            System.out.println(VERDE);
-                            break;
-
-                        case 2:
-                            System.out.println(AZUL);
-                            break;
-
-                        case 3:
-                            System.out.println(ROJO);
-                            break;
-
-                        case 4:
-                            System.out.println(AMARILLO);
-                            break;
-
-                        case 5:
-                            System.out.println(FONDO_AZUL);
-                            break;
-
-                        case 6:
-                            System.out.println(RESET);
-                            break;
-                    
-                        default:
-                            System.out.println("Opción no válida");
-                            break;
-                    }
+                    Function.Color();
+                    //TODO 
+                    break;
 
                 default:
                     Function.mensa("Elige una de las opciones que se dan.\n");
