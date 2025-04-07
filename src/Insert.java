c8deec1c5064d735f41b0e1099d63d2e22bec896
MDR57 <riomardiego@gmail.com>
2025-04-07 11:59:54 +0200

    inserts2 (no va)

--- src/Insert.java
+++ src/Insert.java
@@ -34,26 +34,6 @@ public class Insert {
         String seguridad_social = sc.nextLine();
 
         String sql = "INSERT INTO usuarios (nombre, apellido, direccion, telefono, correo, usuario, contraseña, seguridad_social) " +
-                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-
-        try {
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setString(1, nombre);
-            stmt.setString(2, apellido);
-            stmt.setString(3, direccion);
-            stmt.setString(4, telefono);
-            stmt.setString(5, correo);
-            stmt.setString(6, usuario);
-            stmt.setString(7, contraseña);
-            stmt.setString(8, seguridad_social);
-
-            int filas = stmt.executeUpdate();
-            if (filas > 0) {
-                System.out.println("Registro insertado correctamente.");
-            }
-
-        } catch (SQLException e) {
-            System.out.println("❌ Error al insertar: " + e.getMessage());
-        }
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
     }
 }
